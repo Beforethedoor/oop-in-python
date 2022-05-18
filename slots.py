7b2476eea1d70964331ac0331776243a0deabdf5
Sergei Iniakin <inyakin97@gmail.com>
2022-05-18 11:50:20 +0300

    add slots

---
+++ slots.py
@@ -0,0 +1,42 @@
+from timeit import timeit
+
+
+class Point:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+
+class PointSlot:
+    __slots__ = ("x", "y")
+
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+
+# Класс без определения __slots__ занимает больше памяти
+p = Point(1, 2)
+print(p.__sizeof__(), p.__dict__.__sizeof__())
+# Класс с __slots__ не содержит __dict__
+s = PointSlot(1, 2)
+print(p.__sizeof__())
+
+
+def make_c1():
+    c = Point(1, 2)
+    c.x = 100
+    c.x
+    del c.x
+
+
+def make_c2():
+    c = PointSlot(1, 2)
+    c.x = 100
+    c.x
+    del c.x
+
+
+# Класс с определенным __slots__ работает быстрее
+print(f"Время работы объекта класса Point {timeit(make_c1)}")
+print(f"Время работы объекта класса PointSlot {timeit(make_c2)}")
